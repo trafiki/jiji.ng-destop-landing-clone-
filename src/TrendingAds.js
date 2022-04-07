634f364f002ca8cb89be208da3f0382d2b9daf4f
Babatunde Akerele <babsake@gmail.com>
2022-04-07 12:03:00 +0100

    change links

--- src/TrendingAds.js
+++ src/TrendingAds.js
@@ -1,10 +1,12 @@
-import Ad from './Ad';
+import Ad from "./Ad";
 
 function TrendingAds(props) {
-  const {ads} = props;
+  const { ads } = props;
   return (
     <ul class="trending-ads">
-      {ads.map(ad => <Ad {...ad} />)}
+      {ads.map((ad) => (
+        <Ad {...ad} />
+      ))}
     </ul>
   );
 }
@@ -14,148 +16,172 @@ TrendingAds.defaultProps = {
     {
       name: "Toyota Camry 2008 2.4 LE Silver",
       price: "₦ 1,400,000",
-      image: "https://pictures-nigeria.jijistatic.com/77203580_MTE4OC0yMjA4LTRhNTQyZDc3MzQ.webp",
-      numberOfImages: "9"
+      image:
+        "https://bidfax.info/uploads/posts/2021-05/21/toyota-camry-ce-2008-4t4be46k78r028765-img1.jpg",
+      numberOfImages: "9",
     },
     {
       name: "Toyota Corolla 2007 S Gray",
       price: "₦ 1,330,000",
-      image: "https://pictures-nigeria.jijistatic.com/77336601_MzAwLTIyNS1jZDNiNDYxZjBl.jpg",
-      numberOfImages: "2"
+      image:
+        "https://pictures-nigeria.jijistatic.com/77336601_MzAwLTIyNS1jZDNiNDYxZjBl.jpg",
+      numberOfImages: "2",
     },
     {
       name: "Hyundai Accent 2013 SE Silver",
       price: "₦ 2,400,000",
-      image: "https://pictures-nigeria.jijistatic.com/77399499_MzAwLTQwMC02OTg0MjMwY2Y3.jpg",
-      numberOfImages: "8"
+      image:
+        "https://pictures-nigeria.jijistatic.com/77399499_MzAwLTQwMC02OTg0MjMwY2Y3.jpg",
+      numberOfImages: "8",
     },
     {
       name: "Toyota Camry 2008 2.4 LE Silver",
       price: "₦ 1,400,000",
-      image: "https://pictures-nigeria.jijistatic.com/77398880_MzAwLTM4MC0yNmVhZThlYTdi.jpg",
-      numberOfImages: "9"
+      image:
+        "https://pictures-nigeria.jijistatic.com/77398880_MzAwLTM4MC0yNmVhZThlYTdi.jpg",
+      numberOfImages: "9",
     },
     {
       name: "Toyota Camry 2008 2.4 LE Silver",
       price: "₦ 1,400,000",
-      image: "https://pictures-nigeria.jijistatic.com/75153353_MzAwLTI0MC1kMjA2ZGU1MmZm.jpg",
-      numberOfImages: "9"
+      image:
+        "https://bidfax.info/uploads/posts/2021-05/21/toyota-camry-ce-2008-4t4be46k78r028765-img1.jpg",
+      numberOfImages: "9",
     },
     {
       name: "Toyota Camry 2008 2.4 LE Silver",
       price: "₦ 1,400,000",
-      image: "https://pictures-nigeria.jijistatic.com/77311893_MzAwLTQwMC0yNTQ0OWUzOTJl.jpg",
-      numberOfImages: "9"
+      image:
+        "https://pictures-nigeria.jijistatic.com/77311893_MzAwLTQwMC0yNTQ0OWUzOTJl.jpg",
+      numberOfImages: "9",
     },
     {
       name: "Toyota Camry 2008 2.4 LE Silver",
       price: "₦ 1,400,000",
-      image: "https://pictures-nigeria.jijistatic.com/77349830_MzAwLTQwMC0xZTgwMjZlZjM2.jpg",
-      numberOfImages: "9"
+      image:
+        "https://pictures-nigeria.jijistatic.com/77349830_MzAwLTQwMC0xZTgwMjZlZjM2.jpg",
+      numberOfImages: "9",
     },
     {
       name: "Toyota Camry 2008 2.4 LE Silver",
       price: "₦ 1,400,000",
-      image: "https://pictures-nigeria.jijistatic.com/77310141_MzAwLTQwMC1iMDhkOGE0YjQ0.jpg",
-      numberOfImages: "9"
+      image:
+        "https://pictures-nigeria.jijistatic.com/77310141_MzAwLTQwMC1iMDhkOGE0YjQ0.jpg",
+      numberOfImages: "9",
     },
     {
       name: "Toyota Camry 2008 2.4 LE Silver",
       price: "₦ 1,400,000",
-      image: "https://pictures-nigeria.jijistatic.com/77329612_MzAwLTQwMC02OWM3MGY1NmEx.jpg",
-      numberOfImages: "9"
+      image:
+        "https://bidfax.info/uploads/posts/2021-05/21/toyota-camry-ce-2008-4t4be46k78r028765-img1.jpg",
+      numberOfImages: "9",
     },
     {
       name: "Toyota Camry 2008 2.4 LE Silver",
       price: "₦ 1,400,000",
-      image: "https://pictures-nigeria.jijistatic.com/77351629_MzAwLTQwMC04ZmVlMzM3MDMx.jpg",
-      numberOfImages: "9"
+      image:
+        "https://pictures-nigeria.jijistatic.com/77351629_MzAwLTQwMC04ZmVlMzM3MDMx.jpg",
+      numberOfImages: "9",
     },
     {
       name: "Toyota Camry 2008 2.4 LE Silver",
       price: "₦ 1,400,000",
-      image: "https://pictures-nigeria.jijistatic.com/77411833_MzAwLTQwMC05NjA3YzRjNzEz.jpg",
-      numberOfImages: "9"
+      image:
+        "https://pictures-nigeria.jijistatic.com/77411833_MzAwLTQwMC05NjA3YzRjNzEz.jpg",
+      numberOfImages: "9",
     },
     {
       name: "Toyota Camry 2008 2.4 LE Silver",
       price: "₦ 1,400,000",
-      image: "https://pictures-nigeria.jijistatic.com/77364392_MzAwLTQwMC1mZTdlNzA4MmVh.jpg",
-      numberOfImages: "9"
+      image:
+        "https://pictures-nigeria.jijistatic.com/77364392_MzAwLTQwMC1mZTdlNzA4MmVh.jpg",
+      numberOfImages: "9",
     },
     {
       name: "Toyota Camry 2008 2.4 LE Silver",
       price: "₦ 1,400,000",
-      image: "https://pictures-nigeria.jijistatic.com/67492355_MzAwLTQwMC1jMzc4MTU1MTll.jpg",
-      numberOfImages: "9"
+      image:
+        "https://pictures-nigeria.jijistatic.com/67492355_MzAwLTQwMC1jMzc4MTU1MTll.jpg",
+      numberOfImages: "9",
     },
     {
       name: "Furnished Mini Flat in Alimosho for Rent",
       price: "₦ 1,400,000",
-      image: "https://pictures-nigeria.jijistatic.com/77305044_MzAwLTIyNS1iNjYzYmRiMGZk.jpg",
-      numberOfImages: "9"
+      image:
+        "https://pictures-nigeria.jijistatic.com/77305044_MzAwLTIyNS1iNjYzYmRiMGZk.jpg",
+      numberOfImages: "9",
     },
     {
       name: "Toyota Camry 2008 2.4 LE Silver",
       price: "₦ 1,400,000",
-      image: "https://pictures-nigeria.jijistatic.com/77203580_MTE4OC0yMjA4LTRhNTQyZDc3MzQ.webp",
-      numberOfImages: "9"
+      image:
+        "https://bidfax.info/uploads/posts/2021-05/21/toyota-camry-ce-2008-4t4be46k78r028765-img1.jpg",
+      numberOfImages: "9",
     },
     {
       name: "Toyota Camry 2008 2.4 LE Silver",
       price: "₦ 1,400,000",
-      image: "https://pictures-nigeria.jijistatic.com/77203580_MTE4OC0yMjA4LTRhNTQyZDc3MzQ.webp",
-      numberOfImages: "9"
+      image:
+        "https://bidfax.info/uploads/posts/2021-05/21/toyota-camry-ce-2008-4t4be46k78r028765-img1.jpg",
+      numberOfImages: "9",
     },
     {
       name: "Toyota Camry 2008 2.4 LE Silver",
       price: "₦ 1,400,000",
-      image: "https://pictures-nigeria.jijistatic.com/77203580_MTE4OC0yMjA4LTRhNTQyZDc3MzQ.webp",
-      numberOfImages: "9"
+      image:
+        "https://bidfax.info/uploads/posts/2021-05/21/toyota-camry-ce-2008-4t4be46k78r028765-img1.jpg",
+      numberOfImages: "9",
     },
     {
       name: "Toyota Corolla 2007 S Gray",
       price: "₦ 1,330,000",
-      image: "https://pictures-nigeria.jijistatic.com/77336601_MzAwLTIyNS1jZDNiNDYxZjBl.jpg",
-      numberOfImages: "2"
+      image:
+        "https://pictures-nigeria.jijistatic.com/77336601_MzAwLTIyNS1jZDNiNDYxZjBl.jpg",
+      numberOfImages: "2",
     },
     {
       name: "Hyundai Accent 2013 SE Silver",
       price: "₦ 2,400,000",
-      image: "https://pictures-nigeria.jijistatic.com/77399499_MzAwLTQwMC02OTg0MjMwY2Y3.jpg",
-      numberOfImages: "8"
+      image:
+        "https://pictures-nigeria.jijistatic.com/77399499_MzAwLTQwMC02OTg0MjMwY2Y3.jpg",
+      numberOfImages: "8",
     },
     {
       name: "Toyota Camry 2008 2.4 LE Silver",
       price: "₦ 1,400,000",
-      image: "https://pictures-nigeria.jijistatic.com/77398880_MzAwLTM4MC0yNmVhZThlYTdi.jpg",
-      numberOfImages: "9"
+      image:
+        "https://pictures-nigeria.jijistatic.com/77398880_MzAwLTM4MC0yNmVhZThlYTdi.jpg",
+      numberOfImages: "9",
     },
     {
       name: "Toyota Camry 2008 2.4 LE Silver",
       price: "₦ 1,400,000",
-      image: "https://pictures-nigeria.jijistatic.com/75153353_MzAwLTI0MC1kMjA2ZGU1MmZm.jpg",
-      numberOfImages: "9"
+      image:
+        "https://bidfax.info/uploads/posts/2021-05/21/toyota-camry-ce-2008-4t4be46k78r028765-img1.jpg",
+      numberOfImages: "9",
     },
     {
       name: "Toyota Camry 2008 2.4 LE Silver",
       price: "₦ 1,400,000",
-      image: "https://pictures-nigeria.jijistatic.com/77311893_MzAwLTQwMC0yNTQ0OWUzOTJl.jpg",
-      numberOfImages: "9"
+      image:
+        "https://pictures-nigeria.jijistatic.com/77311893_MzAwLTQwMC0yNTQ0OWUzOTJl.jpg",
+      numberOfImages: "9",
     },
     {
       name: "Toyota Camry 2008 2.4 LE Silver",
       price: "₦ 1,400,000",
-      image: "https://pictures-nigeria.jijistatic.com/77349830_MzAwLTQwMC0xZTgwMjZlZjM2.jpg",
-      numberOfImages: "9"
+      image:
+        "https://pictures-nigeria.jijistatic.com/77349830_MzAwLTQwMC0xZTgwMjZlZjM2.jpg",
+      numberOfImages: "9",
     },
     {
       name: "Toyota Camry 2008 2.4 LE Silver",
       price: "₦ 1,400,000",
-      image: "https://pictures-nigeria.jijistatic.com/77310141_MzAwLTQwMC1iMDhkOGE0YjQ0.jpg",
-      numberOfImages: "9"
-    }
-  ]
-}
+      image:
+        "https://pictures-nigeria.jijistatic.com/77310141_MzAwLTQwMC1iMDhkOGE0YjQ0.jpg",
+      numberOfImages: "9",
+    },
+  ],
+};
 
 export default TrendingAds;
